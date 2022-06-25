acabd0a1605be442c26f1e1ea2de3b48ab723029
Salvation Kemuel <skemuel007@gmail.com>
2022-06-25 19:31:16 +0100

    Database module configured

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -8,35 +8,35 @@ import {
   Put,
 } from '@nestjs/common';
 import { PostService } from './post.service';
-import { CreatePostDto } from './createPost.dto';
-import { UpdatePostDto } from './updatePost.dto';
+import { CreatePostDto } from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
 
 @Controller('posts')
 export class PostController {
   constructor(private readonly postsService: PostService) {}
 
   @Get()
-  getAllPosts() {
-    return this.postsService.getAllPosts();
+  async getAllPosts() {
+    return await this.postsService.getAllPosts();
   }
 
   @Get(':id')
-  getPostById(@Param('id') id: string) {
-    return this.postsService.getPostById(Number(id));
+  async getPostById(@Param('id') id: string) {
+    return await this.postsService.getPostById(Number(id));
   }
 
   @Post()
   async createPost(@Body() post: CreatePostDto) {
-    return this.postsService.createPost(post);
+    return await this.postsService.createPost(post);
   }
 
   @Put(':id')
   async updatePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
-    return this.postsService.replacePost(Number(id), post);
+    return await this.postsService.updatePost(Number(id), post);
   }
 
   @Delete(':id')
   async deletePost(@Param('id') id: string) {
-    return this.postsService.deletePosts(Number(id));
+    return await this.postsService.deletePost(Number(id));
   }
 }
